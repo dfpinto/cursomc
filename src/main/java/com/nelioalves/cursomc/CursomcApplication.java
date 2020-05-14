1042d16862d72ba9f41bb1740856ec30b2761519
Dirley Pinto <dirley.figueredo@gmail.com>
2020-05-14 00:23:33 -0300

    Gravando primeiro arquivo no S3

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,19 +1,25 @@
 package com.nelioalves.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service servS3;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		servS3.uploadFile("D:\\Dirley\\Fotos\\Eu\\IMG-20180223-WA0065.jpeg");
 	}
 
 }
