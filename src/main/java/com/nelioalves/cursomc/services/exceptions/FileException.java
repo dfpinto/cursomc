3cbf098a42444f2f77d6e3ef8ebea554bffd6f6d
Dirley Pinto <dirley.figueredo@gmail.com>
2020-05-16 15:00:05 -0300

    Tratando excecoes adequadamente

---
+++ src/main/java/com/nelioalves/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FileException(String message) {
+		super(message);
+	}
+}
